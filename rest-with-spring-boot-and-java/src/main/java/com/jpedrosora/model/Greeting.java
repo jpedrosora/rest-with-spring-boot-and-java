1dd291f7b68a5492309553d91cb62573c20090ba
Joao Cantuario <pedro.rissardi@gmail.com>
2025-08-11 23:12:49 -0300

    First  Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java/src/main/java/com/jpedrosora/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.jpedrosora.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java/src/test/java/com/jpedrosora/StartupTests.java
@@ -0,0 +1,13 @@
+package com.jpedrosora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
